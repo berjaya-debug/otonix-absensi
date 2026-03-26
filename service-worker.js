fd36eaa41385551a7f4474f4065eab37ed029e0a
berjaya-debug <awalbermulacafe@gmail.com>
2026-03-26 12:24:00 +0700

    Update and rename sw (1).js to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'absen-cbb-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'https://fonts.googleapis.com/css2?family=Cinzel:wght@500;700&family=Nunito:wght@400;600;700;800&display=swap',
+  'https://cdn.jsdelivr.net/npm/sweetalert2@11'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
